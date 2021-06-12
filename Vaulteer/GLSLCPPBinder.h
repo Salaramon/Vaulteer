926981aca145d9e868cfc4b6e26faa738d3c2813
Salaramon <danske.kr@hotmail.com>
2021-06-13 02:25:12 +0900

    Added and linked ShaderBinder

---
+++ Vaulteer/GLSLCPPBinder.h
@@ -0,0 +1,20 @@
+namespace sb {
+	namespace in {
+		const char* aPos = "aPos";
+		const char* aNormal = "aNormal";
+		const char* aTexCoords = "aTexCoords";
+		const char* input0 = "input0";
+		const char* input1 = "input1";
+		const char* input2 = "input2";
+			
+};
+	namespace uniforms {
+		const char* view = "view";
+		const char* projection = "projection";
+		const char* model = "model";
+		const char* uniform0 = "uniform0";
+		const char* uniform1 = "uniform1";
+		const char* uniform2 = "uniform2";
+		
+	};
+};
